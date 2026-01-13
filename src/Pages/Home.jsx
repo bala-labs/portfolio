33b986a8bf1c15d13ec130d2122a48a3acd4e249
Bala <balakrishnan.connect@gmail.com>
2026-01-13 16:54:47 +0530

    Added SmartChat on my Projects

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,10 +12,18 @@ const projects = [
     {
         id: "p2",
         title: "FITNEST",
-        description: "FitNest is a modern gym focused on strength, fitness, and a healthier lifestyle.",
+        description: "FitNest is a modern GYM focused on strength, fitness, and a healthier lifestyle.",
         tech: "HTML, CSS, JS",
         link: "https://bala-labs.github.io/fitnest",
         src: `${process.env.PUBLIC_URL}/assets/fitnest.jpg`
+    }, 
+    {
+        id: "p3",
+        title: "SMARTCHAT",
+        description: "SmartChat is a Chat Application which is used to response to user queries.",
+        tech: "React, CSS, FastApi (Python)",
+        link: "https://smartchat-dev.vercel.app",
+        src: `${process.env.PUBLIC_URL}/assets/smartchat.jpeg`
     }
 ];
 
